d310131027b8132b91ba7f91df487cc1f76e4563
unknown <mquintas@BALT-1006.CORPIT.NSI.NET>
2015-01-27 16:03:49 -0300

    Jump Frog 100/100

--- src/com/maq/codility/FrogJmp.java
+++ src/com/maq/codility/FrogJmp.java
@@ -8,16 +8,14 @@ public class FrogJmp {
     public static void main(String[] args) {
 
         solution(10, 85, 30);
+        solution2(10, 85, 30);
+        solution3(10, 85, 30);
 
     }
 
     /**
      * First approach. Not efficient. 44/100
      * https://codility.com/demo/results/demo9TPBB6-V48/
-     * @param X
-     * @param Y
-     * @param D
-     * @return
      */
     private static int solution(int X, int Y, int D) {
 
@@ -29,6 +27,32 @@ public class FrogJmp {
         }
         System.out.println(contador);
         return contador;
+    }
+
+    /**
+     * 55/100
+     * https://codility.com/demo/results/demoD2CXUH-3XD/
+     */
+    private static int solution2(int X, int Y, int D) {
+
+        int contador =  (int) Math.ceil((Y - X) / (float)D);
+        int suma = X+(D*contador);
+        while (suma < Y) {
+            suma = suma + D;
+            contador++;
+        }
+        System.out.println(contador);
+        return contador;
+    }
+
+    /**
+     * 100/100
+     * https://codility.com/demo/results/demoAB65TJ-XTB/
+     */
+    public static int solution3(int X, int Y, int D) {
+
+        return (int) Math.ceil((Y - X) / (float)D);
+
     }
 
 }
